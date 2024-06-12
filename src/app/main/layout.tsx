d36c898240bb20df434deb2ab82aa07148daf351
yoonsooho <7552678@naver.com>
2024-06-12 14:47:57 +0900

    feat : 메타데이터 변경

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "뭐먹지?",
-    description: "뭐먹지 메인페이지",
+    description: "뭘먹으면 좋을지 추천해주는 사이트입니다.",
     openGraph: {
         images: "/img/LocationIcon.svg",
     },
