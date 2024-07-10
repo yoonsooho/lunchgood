0f33a746e858c3e3d6f62b97c277dc14038d4a2a
yoonsooho <7552678@naver.com>
2024-07-10 13:13:58 +0900

    chore : test

--- components/Main.tsx
+++ components/Main.tsx
@@ -232,6 +232,7 @@ export default function Main() {
             searchPlaces(search.pageNation.current + 1);
         }
     }, [inView]);
+
     // useEffect(() => {
     //     if (!isSdkLoaded) return;
     //     const { kakao } = window;
